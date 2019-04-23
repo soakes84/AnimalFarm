2b0e74cd636f6abcdf2570a667d4a5a56a16389b
soakes84 <spencer.oakes84@gmail.com>
2019-04-22 20:44:35 -0400

    FarmContext and connection string

---
+++ AnimalFarm/Data/FarmContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AnimalFarm.Data
+{
+    public class FarmContext : DbContext
+    {
+        public FarmContext(DbContextOptions<FarmContext> options) : base(options)
+        {
+            Database.Migrate();
+        }
+
+        public DbSet<Animal> Animals { get; set; }
+    }
+}
--- AnimalFarm/Startup.cs
+++ AnimalFarm/Startup.cs
@@ -2,10 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AnimalFarm.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -22,13 +24,13 @@ namespace AnimalFarm
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddDbContext<FarmContext>(op => op.UseSqlServer(Configuration["ConnectionString:AnimalFarmDB"]));
+
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
