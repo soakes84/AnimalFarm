daca26d86515b3e130b53db59dae81b126297301
soakes84 <spencer.oakes84@gmail.com>
2019-04-22 20:39:03 -0400

    initial, animal class

---
+++ AnimalFarm/Data/Animal.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AnimalFarm.Data
+{
+    [Table("Animal", Schema = "dbo")]
+    public class Animal
+    {
+        [Key]
+
+        public Guid AnimalId { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Age { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Gender { get; set; }
+
+        [Required]
+        public bool Friendly { get; set; }
+
+        [Required]
+        public string Food { get; set; }
+
+        [Required]
+        public int Health { get; set; }
+
+    }
+}
